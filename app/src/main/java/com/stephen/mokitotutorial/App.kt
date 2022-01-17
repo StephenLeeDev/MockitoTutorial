618097753180424b1e1ea47ea745a84e39bc3812
StephenLeeDev <stephen.lee.dev@gmail.com>
2022-01-17 17:03:45 +0900

    Add | Setup Application

---
+++ app/src/main/java/com/stephen/mokitotutorial/App.kt
@@ -0,0 +1,24 @@
+package com.stephen.mokitotutorial
+
+import android.app.Application
+import com.stephen.mokitotutorial.di.diModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+/**
+ * Written by StephenLeeDev on 2022/01/17.
+ */
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@App)
+            modules(diModule)
+        }
+    }
+}
---
+++ app/src/main/java/com/stephen/mokitotutorial/di/DiModule.kt
@@ -0,0 +1,10 @@
+package com.stephen.mokitotutorial.di
+
+import org.koin.dsl.module
+
+/**
+ * Written by StephenLeeDev on 2022/01/17.
+ */
+internal val diModule = module {
+
+}
