8ceb9f7bae30ba2b5a74ea3c316c94387136c6b0
StephenLeeDev <stephen.lee.dev@gmail.com>
2022-01-20 22:31:43 +0900

    Add | Generate ToDo Data Access Object Class

---
+++ app/src/main/java/com/stephen/mokitotutorial/db/ToDoDao.kt
@@ -0,0 +1,34 @@
+package com.stephen.mokitotutorial.db
+
+import androidx.room.*
+import com.stephen.mokitotutorial.data.model.ToDoModel
+
+/**
+ * Written by StephenLeeDev on 2022/01/20.
+ */
+
+@Dao
+interface ToDoDao {
+
+ @Query("SELECT * FROM ToDoModel")
+ suspend fun getAll(): List<ToDoModel>
+
+ @Query("SELECT * FROM ToDoModel WHERE id=:id")
+ suspend fun getById(id: Long): ToDoModel?
+
+ @Insert
+ suspend fun insert(toDoEntity: ToDoModel): Long
+
+ @Insert(onConflict = OnConflictStrategy.REPLACE)
+ suspend fun insert(toDoEntityList: List<ToDoModel>)
+
+ @Query("DELETE FROM ToDoModel WHERE id=:id")
+ suspend fun delete(id: Long)
+
+ @Query("DELETE FROM ToDoModel")
+ suspend fun deleteAll()
+
+ @Update
+ suspend fun update(toDoEntity: ToDoModel)
+
+}
