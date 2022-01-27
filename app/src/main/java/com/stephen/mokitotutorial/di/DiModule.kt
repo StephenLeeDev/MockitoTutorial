4943426dec34d7ca4e64ef995bf42f400dedf7af
StephenLeeDev <stephen.lee.dev@gmail.com>
2022-01-27 23:03:19 +0900

    Add | Setup Koin DI

--- app/src/main/java/com/stephen/mokitotutorial/di/DiModule.kt
+++ app/src/main/java/com/stephen/mokitotutorial/di/DiModule.kt
@@ -1,5 +1,22 @@
 package com.stephen.mokitotutorial.di
 
+import android.content.Context
+import androidx.room.Room
+import com.stephen.mokitotutorial.data.repository.DefaultToDoRepository
+import com.stephen.mokitotutorial.data.repository.ToDoRepository
+import com.stephen.mokitotutorial.db.ToDoDatabase
+import com.stephen.mokitotutorial.domain.todo.*
+import com.stephen.mokitotutorial.domain.todo.DeleteToDoItemUseCase
+import com.stephen.mokitotutorial.domain.todo.GetToDoItemUseCase
+import com.stephen.mokitotutorial.domain.todo.GetToDoListUseCase
+import com.stephen.mokitotutorial.domain.todo.InsertToDoListUseCase
+import com.stephen.mokitotutorial.domain.todo.InsertToDoUseCase
+import com.stephen.mokitotutorial.presenter.detail.DetailMode
+import com.stephen.mokitotutorial.presenter.detail.DetailViewModel
+import com.stephen.mokitotutorial.presenter.list.ListViewModel
+import kotlinx.coroutines.Dispatchers
+import org.koin.android.ext.koin.androidApplication
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 /**
@@ -8,4 +25,28 @@ import org.koin.dsl.module
 
 internal val diModule = module {
 
+    single { Dispatchers.Main }
+    single { Dispatchers.IO }
+
+    factory { GetToDoListUseCase(get()) }
+    factory { GetToDoItemUseCase(get()) }
+    factory { InsertToDoListUseCase(get()) }
+    factory { InsertToDoUseCase(get()) }
+    factory { DeleteToDoItemUseCase(get()) }
+    factory { DeleteAllToDoItemUseCase(get()) }
+    factory { UpdateToDoUseCase(get()) }
+
+    single<ToDoRepository> { DefaultToDoRepository(get(), get()) }
+
+    single { provideDB(androidApplication()) }
+    single { provideToDoDao(get()) }
+
+    viewModel { ListViewModel(get(), get(), get()) }
+    viewModel { (detailMode: DetailMode, id: Long) -> DetailViewModel(detailMode, id, get(), get(), get(), get()) }
+
 }
+
+internal fun provideDB(context: Context): ToDoDatabase =
+    Room.databaseBuilder(context, ToDoDatabase::class.java, ToDoDatabase.DB_NAME).build()
+
+internal fun provideToDoDao(database: ToDoDatabase) = database.toDoDao()
