6e5feb633a8d46577eb68fc6a1dbf27e4a13528e
StephenLeeDev <stephen.lee.dev@gmail.com>
2022-02-02 17:45:19 +0900

    Rename | Rename MainActivity to ListActivity

--- app/src/main/java/com/stephen/mokitotutorial/presenter/list/ListActivity.kt
+++ app/src/main/java/com/stephen/mokitotutorial/presenter/list/ListActivity.kt
@@ -1,11 +1,12 @@
-package com.stephen.mokitotutorial
+package com.stephen.mokitotutorial.presenter.list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.stephen.mokitotutorial.R
 
-class MainActivity : AppCompatActivity() {
+class ListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_list)
     }
 }
