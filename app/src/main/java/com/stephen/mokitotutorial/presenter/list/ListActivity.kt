f1cb076460bc082f7585aadccaad8aac660a957a
StephenLeeDev <stephen.lee.dev@gmail.com>
2022-02-08 23:05:30 +0900

    Adding | Conducting ToDo List Screen

--- app/src/main/java/com/stephen/mokitotutorial/presenter/list/ListActivity.kt
+++ app/src/main/java/com/stephen/mokitotutorial/presenter/list/ListActivity.kt
@@ -1,12 +1,34 @@
 package com.stephen.mokitotutorial.presenter.list
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.stephen.mokitotutorial.R
+import com.stephen.mokitotutorial.databinding.ActivityListBinding
+import com.stephen.mokitotutorial.presenter.BaseActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.coroutines.CoroutineContext
 
-class ListActivity : AppCompatActivity() {
+internal class ListActivity : BaseActivity<ListViewModel>(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + Job()
+
+    private val binding: ActivityListBinding by lazy {
+        ActivityListBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var adapter: ToDoListAdapter
+
+    override val viewModel: ListViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+    }
+
+    override fun observeData() {
+
     }
 }
